5c0bb6d03181fb506e343a86509832df9423097e
unknown <barhancet@gamingcampus.local>
2023-11-06 18:01:47 +0100

    debut de classe GO

--- Casse-brique/GameObject.cpp
+++ Casse-brique/GameObject.cpp
@@ -1,10 +1,40 @@
 #include "GameObject.h"
+#include <SFML/Graphics.hpp>
 
-GameObject::GameObject()
+using namespace sf;
+
+GameObject::GameObject(char* type, RenderWindow* renderer)
 {
 	m_positionX = 0;
 	m_positionY = 0;
-	m_width = 5;
-	m_height = 10;
+	m_width = 1;
+	m_height = 1;
+	m_renderer = renderer;
 
+	//shape can be a rectangle or a circle
+	if (type == "rectangle") {
+		m_shape = new RectangleShape(Vector2<float>(m_width, m_height));
+	}
+	else if (type == "circle") {
+		m_shape = new CircleShape(m_width);
+	}
+	this->SetSize(50, 50);
+	
 };
+
+void GameObject::SetPosition(float x, float y)
+{
+	this->m_positionX = x;
+	this->m_positionY = y;
+	this->m_shape->setPosition(x, y);
+};
+
+void GameObject::SetSize(float w, float h) 
+{
+	this->m_shape->setScale(h, w);
+}
+
+void GameObject::Draw()
+{
+	this->m_renderer->draw(this->m_shape);
+}
--- Casse-brique/GameObject.h
+++ Casse-brique/GameObject.h
@@ -3,16 +3,24 @@
 namespace sf
 {
 	class Shape;
+	class RenderWindow;
 }
 
 class GameObject
 {
 public:
-	int m_positionX;
-	int m_positionY;
-	int m_width;
-	int m_height;
-	sf::Shape* shape;
+	float m_positionX;
+	float m_positionY;
+	float m_width;
+	float m_height;
+	sf::Shape* m_shape;
+	sf::RenderWindow* m_renderer;
 
-	GameObject();
+	GameObject(char* type, sf::RenderWindow* renderer);
+
+	void SetPosition(float x, float y);
+
+	void SetSize(float w, float h);
+
+	void Draw();
 };
